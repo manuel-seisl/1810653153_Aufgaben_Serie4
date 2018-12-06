bb3d5a4631087b08620bebd86db49926b937f80c
manuel-seisl <1810653153@stud.fh-kufstein.ac.at>
2018-12-06 09:43:35 +0100

    Aufgabe 1

---
+++ src/at/ac/fh_kufstein/uebung04/Classes/Car.java
@@ -0,0 +1,15 @@
+package at.ac.fh_kufstein.uebung04.Classes;
+
+public class Car
+{
+
+    private String color;
+    private int wheels;
+    private double speed;
+
+    public void currentSpeed()
+    {
+        System.out.println(this.speed);
+    }
+
+}
---
+++ src/at/ac/fh_kufstein/uebung_04/Aufruf.java
@@ -0,0 +1,11 @@
+package at.ac.fh_kufstein.uebung_04;
+
+public class Aufruf
+{
+
+    public static void main(String[] args)
+    {
+
+    }
+
+}
